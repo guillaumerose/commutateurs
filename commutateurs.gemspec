597688a7e157ff0dabc02d7784227114f6085dab
Guillaume Rose <rose@netxp.fr>
2013-02-12 14:31:08 +0100

    Juniper

--- commutateurs.gemspec
+++ commutateurs.gemspec
@@ -1,8 +1,8 @@
 # encoding: utf-8
 Gem::Specification.new do |gem|
   gem.name = "commutateurs"
-  gem.version = "0.0.2"
-  gem.date = "2013-01-01"
+  gem.version = "0.0.3"
+  gem.date = "2013-02-01"
   gem.authors = ["Guillaume Rose"]
   gem.email = "guillaume.rose@gmail.com"
   gem.summary = "Library for accessing commutateurs"
--- lib/commutateurs/device.rb
+++ lib/commutateurs/device.rb
@@ -83,4 +83,29 @@ module Commutateurs
       @transport.close
     end
   end
+
+  class Juniper < Base
+    def initialize(host, credentials, verbose = false)
+      super
+      @transport.default_prompt = /-> $/
+    end
+
+    def enable
+    end
+
+    def connect
+      @transport.connect
+    end
+
+    def configuration
+    end
+
+    def save
+    end
+
+    def disconnect
+      @transport.send 'exit'
+      @transport.close
+    end
+  end
 end
