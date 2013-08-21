9a01a1d81f5ad5ba7599ccd6c3d6c9b891e2abea
Guillaume Rose <rose@netxp.fr>
2013-08-21 15:11:35 +0200

    Ironport support

---
+++ lib/commutateurs/ironport.rb
@@ -0,0 +1,29 @@
+module Commutateurs
+  class Ironport < Base
+    def initialize(host, credentials, verbose = false)
+      super
+      @transport.default_prompt = /> $/
+      @transport.more = /-Press Any Key For More-/
+    end
+
+    def enable
+    end
+
+    def connect
+      @transport.connect
+    end
+
+    def configuration
+      execute('showconfig')
+      execute('Y')
+    end
+
+    def save
+    end
+
+    def disconnect
+      @transport.send 'exit'
+      @transport.close
+    end
+  end
+end
